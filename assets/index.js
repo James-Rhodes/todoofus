ebe2abfdf5d2a6ec821d50f743e1a31e815e4088
James Rhodes <james.rhodes@hotmail.com.au>
2025-09-04 22:26:06 +1000

    Feat: Can now check todos

--- assets/index.js
+++ assets/index.js
@@ -32,7 +32,22 @@ function createTodo(event) {
   submittedForm.closest(".todo-form").replaceWith(newTodo);
 }
 
+function getChildrenTodoItems(parentTodo) {
+  const parentUL = parentTodo.parentElement.parentElement; // The todo item is in a li, which must also be in a ul
+  return parentUL.querySelectorAll(".todo-item");
+}
+
 // TODO: On check of a todo, recursively check all of its children todos
 // TODO: On uncheck of a todo, recursively uncheck all of its children todos
+function toggleTodoItemRecursively(parentTodo) {
+  const checked = parentTodo.querySelector(".todo-checkbox").checked;
+  const childTodos = getChildrenTodoItems(parentTodo);
+
+  // TODO: Actually send this to the DB
+  for (const child of childTodos) {
+    child.querySelector(".todo-checkbox").checked = checked;
+  }
+}
+
 // TODO: checked nodes sorted to the bottom of the ul they are in, when unchecked move them back into the unchecked section
 // TODO: Server should show all todo's from the last X days. Even if they are completed. So they can be unticked as necessary
