fcc1071dab1fcb617224c1eda3eb2c37c199e80b
James Rhodes <james.rhodes@hotmail.com.au>
2025-09-17 01:46:04 +1000

    Feat: added logs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ const DB_PATH: &str = "./data/";
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    tracing::info!("Beginning todoofus");
 
     if let Err(e) = run().await {
         eprintln!("Application error: {:?}", e);
@@ -19,7 +20,9 @@ async fn main() {
 
 async fn run() -> anyhow::Result<()> {
     let db = DB::new(DB_PATH).await?;
+    tracing::info!("Created DB");
     let app = routes::app(db);
+    tracing::info!("Created App");
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
