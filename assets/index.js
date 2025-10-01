7edf24218afe59d80c1e99caeb056598e7d36a35
James Rhodes <james.rhodes@hotmail.com.au>
2025-10-02 00:40:10 +1000

    Feat: added typescript type checking

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,14 @@
-const todoTemplate = document.getElementById("todo-template");
-const todoFormTemplate = document.getElementById("todo-form-template");
-const todoEditFormTemplate = document.getElementById("todo-edit-form-template");
+// @ts-check
+
+const todoTemplate = /** @type {HTMLTemplateElement} */ (
+  document.getElementById("todo-template")
+);
+const todoFormTemplate = /** @type {HTMLTemplateElement} */ (
+  document.getElementById("todo-form-template")
+);
+const todoEditFormTemplate = /** @type {HTMLTemplateElement} */ (
+  document.getElementById("todo-edit-form-template")
+);
 
 const DOUBLE_CLICK_DELAY = 200; // milliseconds
 
@@ -23,46 +31,96 @@ window.addEventListener("load", async () => {
       todo.children,
     );
 
-    todo_container.appendChild(todoElement);
+    todo_container?.appendChild(todoElement);
   }
   adjustLineHeights();
 });
 
-// Places the form immediately after the refNode
+/** Places the form immediately after the refNode
+ * @param {HTMLElement} refNode
+ */
 function newTodoForm(refNode) {
   let parentId = "";
   if (refNode.id != "dummy-first-todo") {
-    parentId = refNode.querySelector("input[name='id']").value;
+    const parIdElement = refNode.querySelector("input[name='id']");
+    if (!(parIdElement instanceof HTMLInputElement)) {
+      throw "element was not an input element";
+    }
+    parentId = parIdElement.value;
   }
 
-  const template = todoFormTemplate.content.cloneNode(true);
-  template.querySelector("input[name='parent_id']").value = parentId;
+  const template = /** @type {DocumentFragment}*/ (
+    todoFormTemplate.content.cloneNode(true)
+  );
+  const parentIdElement = /** @type {HTMLInputElement | null} */ (
+    template.querySelector("input[name='parent_id']")
+  );
+  if (!parentIdElement) {
+    throw "no parent id element in template";
+  }
+  parentIdElement.value = parentId;
+
   refNode.after(template);
-  const descriptionInput = refNode.nextElementSibling.querySelector(
+  const descriptionInput = refNode.nextElementSibling?.querySelector(
     "[name='description']",
   );
+  if (!(descriptionInput instanceof HTMLInputElement)) {
+    throw "no description element";
+  }
+
   descriptionInput.focus(); // Focus for immediate typing
 }
 
+/**
+ * @param {HTMLElement} formElement
+ */
 function removeTodoForm(formElement) {
   formElement.remove();
   adjustLineHeights();
 }
 
+/**
+ * @param {number} id
+ * @param {string} todoDescription
+ * @param {boolean} todoCompleted
+ * @returns {DocumentFragment}
+ */
 function createTodoElement(id, todoDescription, todoCompleted) {
-  const newTodo = todoTemplate.content.cloneNode(true);
+  const newTodo = /** @type {DocumentFragment} */ (
+    todoTemplate?.content.cloneNode(true)
+  );
 
-  newTodo.querySelector(".todo-description").innerHTML =
-    markdownLinksToHtml(todoDescription);
+  const description = newTodo.querySelector(".todo-description");
+  if (!description) {
+    throw "description element not here";
+  }
+  description.innerHTML = markdownLinksToHtml(todoDescription);
 
-  newTodo.querySelector("input[name='id']").value = id;
-  newTodo.querySelector(".todo-checkbox").checked = todoCompleted;
+  /** @type {HTMLInputElement | null}*/
+  const idInput = newTodo.querySelector("input[name='id']");
+  if (!idInput) {
+    throw "id input not here";
+  }
+  idInput.value = id + "";
+
+  /** @type {HTMLInputElement | null}*/
+  const checkboxElement = newTodo.querySelector(".todo-checkbox");
+  if (!checkboxElement) {
+    throw "checkbox element not here";
+  }
+  checkboxElement.checked = todoCompleted;
 
   const content = newTodo.querySelector(".todo-contents");
+  if (!content) {
+    throw "Failed to get todo-contents";
+  }
 
   let clickTimer = null;
   content.addEventListener("click", (event) => {
-    console.log(event.target.tagName);
+    if (!(event.target instanceof HTMLElement)) {
+      return;
+    }
+
     const clickedTagName = event.target.tagName.toLowerCase();
     if (clickedTagName === "a" || clickedTagName === "input") {
       return; // We want to still allow clicking anchor tags and clicking checkbox
@@ -74,7 +132,15 @@ function createTodoElement(id, todoDescription, todoCompleted) {
       clickTimer = null;
 
       console.log("Double Click");
-      newTodoForm(event.target.closest(".todo-item").parentNode); // On double click we create a new todo
+      const closestTodo = event.target.closest(".todo-item");
+      if (!closestTodo) {
+        throw "Failed to get closest todo";
+      }
+      const todoParentNode = closestTodo.parentNode;
+      if (!(todoParentNode instanceof HTMLElement)) {
+        throw "todo parent node was not HTML Element";
+      }
+      newTodoForm(todoParentNode); // On double click we create a new todo
       return;
     }
 
@@ -83,13 +149,28 @@ function createTodoElement(id, todoDescription, todoCompleted) {
       clickTimer = null;
 
       console.log("Single Click");
-      newEditTodoForm(event.target.closest(".todo-item")); // On single click we edit the current todo
+      if (!(event.target instanceof HTMLElement)) {
+        return;
+      }
+      const closestTodoItem = event.target.closest(".todo-item");
+      if (!(closestTodoItem instanceof HTMLElement)) {
+        throw "no closest item";
+      }
+      newEditTodoForm(closestTodoItem); // On single click we edit the current todo
     }, DOUBLE_CLICK_DELAY);
   });
 
   return newTodo;
 }
 
+/**
+ * @param {number} id
+ * @param {string} todoDescription
+ * @param {boolean} todoCompleted
+ * @param {Array<Object>} childTodos
+ *
+ * @returns {HTMLElement}
+ */
 function createTodoTree(id, todoDescription, todoCompleted, childTodos) {
   const todoTreeRoot = document.createElement("ul");
 
@@ -98,7 +179,7 @@ function createTodoTree(id, todoDescription, todoCompleted, childTodos) {
   listItem.appendChild(newTodo);
   todoTreeRoot.appendChild(listItem);
 
-  for (child of childTodos) {
+  for (const child of childTodos) {
     todoTreeRoot.appendChild(
       createTodoTree(
         child.id,
@@ -112,18 +193,35 @@ function createTodoTree(id, todoDescription, todoCompleted, childTodos) {
   return todoTreeRoot;
 }
 
+/**
+ * @param {Event} event
+ */
 async function createTodo(event) {
   event.preventDefault();
 
   const submittedForm = event.target;
-  let parentId = parseInt(
-    submittedForm.querySelector("input[name='parent_id']").value,
+  if (!(submittedForm instanceof HTMLFormElement)) {
+    throw "submitted form was not a form element";
+  }
+
+  const parentIdElement = submittedForm.querySelector(
+    "input[name='parent_id']",
   );
+  if (!(parentIdElement instanceof HTMLInputElement)) {
+    throw "failed to get parent id";
+  }
+
+  /** @type {number|undefined} */
+  let parentId = parseInt(parentIdElement.value);
   parentId = isNaN(parentId) ? undefined : parentId; // If the value is empty then set it to undefined
 
-  const todoDescription = submittedForm.querySelector(
+  const descriptionElement = submittedForm.querySelector(
     ".todo-description-input",
-  ).value;
+  );
+  if (!(descriptionElement instanceof HTMLInputElement)) {
+    throw "failed to get description input";
+  }
+  const todoDescription = descriptionElement.value;
 
   let newTodoId = undefined;
   try {
@@ -145,24 +243,48 @@ async function createTodo(event) {
   }
 
   const newTodo = createTodoTree(newTodoId, todoDescription, false, []);
-  submittedForm.closest(".todo-form").replaceWith(newTodo);
+  const closestTodoForm = submittedForm.closest(".todo-form");
+  if (!(closestTodoForm instanceof HTMLElement)) {
+    throw "failed to get closest todo form";
+  }
+  closestTodoForm.replaceWith(newTodo);
 
   adjustLineHeights();
 }
 
+/**
+ * @param {HTMLElement} parentTodo
+ * @returns {NodeListOf<HTMLElement>}
+ */
 function getChildrenTodoItems(parentTodo) {
-  const parentUL = parentTodo.parentElement.parentElement; // The todo item is in a li, which must also be in a ul
+  const parentUL = parentTodo.parentElement?.parentElement; // The todo item is in a li, which must also be in a ul
+  if (!parentUL) {
+    throw "parentTodo not inside a UL?";
+  }
+
   return parentUL.querySelectorAll(".todo-item");
 }
 
+/**
+ * @param {HTMLElement} parentTodo
+ */
 async function toggleTodoItemRecursively(parentTodo) {
-  const checked = parentTodo.querySelector(".todo-checkbox").checked;
+  const todoCheckbox = parentTodo.querySelector(".todo-checkbox");
+  if (!(todoCheckbox instanceof HTMLInputElement)) {
+    throw "todo checkbox not found";
+  }
+  const checked = todoCheckbox.checked;
   const childTodos = getChildrenTodoItems(parentTodo);
 
   const todoToUpdate = [];
   for (const child of childTodos) {
+    const idElement = child.querySelector("input[name='id']");
+    if (!(idElement instanceof HTMLInputElement)) {
+      throw "id element is not input";
+    }
+
     todoToUpdate.push({
-      id: parseInt(child.querySelector("input[name='id']").value),
+      id: parseInt(idElement.value),
       completed: checked,
     });
   }
@@ -183,24 +305,52 @@ async function toggleTodoItemRecursively(parentTodo) {
   }
 
   for (const child of childTodos) {
-    child.querySelector(".todo-checkbox").checked = checked;
+    const checkBoxElement = child.querySelector(".todo-checkbox");
+    if (!(checkBoxElement instanceof HTMLInputElement)) {
+      throw "checkbox element is not input";
+    }
+    checkBoxElement.checked = checked;
   }
 }
 
+/**
+ * @param {HTMLElement} todoElement
+ */
 function newEditTodoForm(todoElement) {
-  const editForm = todoEditFormTemplate.content.cloneNode(true);
+  const editForm = /** @type {DocumentFragment} */ (
+    todoEditFormTemplate?.content.cloneNode(true)
+  );
 
-  const todoID = todoElement.querySelector("input[name='id']").value;
-  editForm.querySelector("input[name='id']").value = todoID;
+  const idElement = todoElement.querySelector("input[name='id']");
+  if (!(idElement instanceof HTMLInputElement)) {
+    throw "failed to get input id element";
+  }
+  const todoID = idElement.value;
+
+  const idInput = /** @type {HTMLInputElement | null}  */ (
+    editForm.querySelector("input[name='id']")
+  );
+  if (!idInput) {
+    throw "no id input element";
+  }
+  idInput.value = todoID;
 
   const todoDescription = htmlLinksToMarkdown(
-    todoElement.querySelector(".todo-description").innerHTML,
+    todoElement.querySelector(".todo-description")?.innerHTML,
   );
-  const descriptionInput = editForm.querySelector("[name='description']");
+  const descriptionInput = /** @type {HTMLInputElement | null}  */ (
+    editForm.querySelector("[name='description']")
+  );
+  if (!descriptionInput) {
+    throw "no description input";
+  }
   descriptionInput.value = todoDescription;
 
   const todoBbox = todoElement.getBoundingClientRect();
   const submitButtons = editForm.querySelector(".todo-edit-buttons");
+  if (!submitButtons) {
+    throw "no submit buttons found";
+  }
 
   todoElement.replaceWith(editForm);
 
@@ -217,9 +367,14 @@ function newEditTodoForm(todoElement) {
   }
 
   // Make the text area submit form on enter key
-  const formElement = descriptionInput.closest(".todo-item");
-  formElement.style.width = todoBbox.width;
-  formElement.style.height = todoBbox.height;
+  const formElement = /** @type {HTMLFormElement} */ (
+    descriptionInput.closest(".todo-item")
+  );
+  if (!formElement) {
+    throw "no form element";
+  }
+  formElement.style.width = todoBbox.width + "";
+  formElement.style.height = todoBbox.height + "";
   descriptionInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       formElement.requestSubmit();
@@ -227,18 +382,30 @@ function newEditTodoForm(todoElement) {
   });
 }
 
+/**
+ * @param {Event} event
+ */
 async function editTodo(event) {
   event.preventDefault();
 
   const submittedForm = event.target;
+  if (!(submittedForm instanceof HTMLFormElement)) {
+    throw "form was not a form";
+  }
 
-  const newDescription = submittedForm.querySelector(
+  const descriptionElement = submittedForm.querySelector(
     "[name='description']",
-  ).value;
-
-  const todoID = parseInt(
-    submittedForm.querySelector("input[name='id']").value,
   );
+  if (!(descriptionElement instanceof HTMLTextAreaElement)) {
+    throw "description was not an input";
+  }
+  const newDescription = descriptionElement.value;
+
+  const idElement = submittedForm.querySelector("input[name='id']");
+  if (!(idElement instanceof HTMLInputElement)) {
+    throw "idElement was not input";
+  }
+  const todoID = parseInt(idElement.value);
 
   // Send to DB
   let todo_json;
@@ -273,12 +440,19 @@ async function editTodo(event) {
   adjustLineHeights();
 }
 
+/**
+ * @param {HTMLElement} todoItem
+ */
 async function deleteTodo(todoItem) {
   if (!confirm("Are you sure?")) {
     return;
   }
 
-  const todoID = parseInt(todoItem.querySelector("input[name='id']").value);
+  const idElement = todoItem.querySelector("input[name='id']");
+  if (!(idElement instanceof HTMLInputElement)) {
+    throw "id element not input element";
+  }
+  const todoID = parseInt(idElement.value);
 
   // Send to DB
   try {
@@ -297,24 +471,36 @@ async function deleteTodo(todoItem) {
   }
 
   // Update DOM
-  const treeUL = todoItem.parentElement.parentElement;
+  const treeUL = todoItem.parentElement?.parentElement;
+  if (!treeUL) {
+    throw "no parent parent element of todo item";
+  }
+
   treeUL.remove();
 
   adjustLineHeights();
 }
 
 function adjustLineHeights() {
+  /** @type {NodeListOf<HTMLElement>} */
   const lis = document.querySelectorAll("ul ul li");
+
   for (const curr of lis) {
     const sibCheckbox =
-      curr.parentElement.previousElementSibling.querySelector(".todo-checkbox");
+      curr?.parentElement?.previousElementSibling?.querySelector(
+        ".todo-checkbox",
+      );
     if (sibCheckbox == null) {
       continue;
     }
 
     const currRect = curr
-      .querySelector(".todo-checkbox")
-      .getBoundingClientRect();
+      ?.querySelector(".todo-checkbox")
+      ?.getBoundingClientRect();
+
+    if (!currRect) {
+      continue;
+    }
     const currY = currRect.top + currRect.height / 2;
     const currHeight = currRect.height;
 
@@ -331,13 +517,26 @@ function adjustLineHeights() {
   }
 }
 
+/**
+ * @param {string} text
+ * @return {string}
+ */
 function markdownLinksToHtml(text) {
   return text.replace(
     /\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g,
     '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>',
   );
 }
+
+/**
+ * @param {string | undefined} html
+ * @return {string}
+ */
 function htmlLinksToMarkdown(html) {
+  if (!html) {
+    return "";
+  }
+
   return html.replace(/<a\s+href="([^"]+)"[^>]*>(.*?)<\/a>/g, "[$2]($1)");
 }
 
